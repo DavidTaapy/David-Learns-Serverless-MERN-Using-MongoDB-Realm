146d5ce9e7c70e45cd17059cdbe857f7e54fd1be
DavidTaapy <74765863+DavidTaapy@users.noreply.github.com>
2022-01-30 15:17:48 +0800

    Update http-common.js

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://ap-southeast-1.aws.data.mongodb-api.com/app/restaurant-app-ifhpu/endpoint/",    // Base URL for Backend Server
+    // baseURL: "http://localhost:5000/api/v1/restaurants/", // Base URL for Backend
+    baseURL: "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-app-ifhpu/service/restaurants/incoming_webhook/",
+    // baseURL: "https://ap-southeast-1.aws.data.mongodb-api.com/app/restaurant-app-ifhpu/endpoint/", // Base URL for Backend Server - Using HTTPS Endpoints
     headers: {
         "Content-type": "application/json"
     }
